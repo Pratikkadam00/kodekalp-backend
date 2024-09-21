9b0ac1c45d8c6f67e4e9f73beaf424b32f921d9e
Pratikkadam00 <pratikadam00@gmail.com>
2024-09-22 01:38:41 +0530

    updated

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import authRoutes from "./route/authRoutes.js";
+import cors from "cors";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Testing
+app.get("/", (req, res) => {
+  res.send("Healthy");
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+
+// Error Handler
+app.use(errorHandler);
+
+export default app;
